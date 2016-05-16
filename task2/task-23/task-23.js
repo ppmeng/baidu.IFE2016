4cac900ac672e81b547c0b316f4ca797c8025f98
ppmeng <yswdzlg@gmail.com>
2016-05-16 23:41:36 +0800

    DFS,BFS

--- task2/task-23/task-23.js
+++ task2/task-23/task-23.js
@@ -2,16 +2,34 @@ function Tree(node) {
 	this.traversalNodes = [];
 	this.root = node;
 }
-Tree.prototype.DFS = function(node) {
-	if (node) {
-		var head = node.childNodes;
-		for (var i = 0; i < head.length; i++) {
-			console.log(123);
-			if (head[i].nodeType === "1") {
-				tree.traversalNodes.push(head[i]);
-				DFS(head[i]);
-			}
-		}
-	}
-}
+
+/**
+ * DFS深度优先搜索
+ */
+Tree.prototype.DFS = function(callback) {
+	var list = [];
+	(function recurse(currentNode) {
+        list.push(currentNode);
+        for (var i = 0; i < currentNode.children.length; i++) {
+        	recurse(curentNode.children[i]);
+        }
+        callback ? callback(currentNode) : null;
+	})(this.root);
+	this.traversalNodes = list;
+};
+
+Tree.prototype.BFS = function(callback) {
+    var list = [],
+        currentNode = this.root;
+    this.traversalNodes = [];
+    this.traversalNodes.push(currentNode);
+    while (currentNode) {
+    	var len = currentNode.childNodes.length;
+    	for (var i = 0; i < len; i++) {
+    		list.push(currentNode.childNodes[i]);
+    	}
+    	callback ? callback(currentNode) : null;
+    	currentNode = list.shift();
+    }
+};
 
